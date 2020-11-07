7456a503e6acff740fffb23dff13bf2df8b1ce72
HangorG <gaba93@yandex.ru>
2020-11-08 00:16:48 +0300

    delete http req

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { ui } from "./ui";
 
 document.addEventListener("DOMContentLoaded", getPosts);
 document.querySelector(".post-submit").addEventListener("click", submitPost);
+document.querySelector("#posts").addEventListener("click", deletePost);
 
 function getPosts() {
   http.get("http://localhost:3000/posts")
@@ -11,19 +12,35 @@ function getPosts() {
 }
 
 function submitPost() {
-  const title = document.querySelector('#title').value;
-  const body = document.querySelector('#body').value;
+  const title = document.querySelector("#title").value;
+  const body = document.querySelector("#body").value;
 
   const data = {
     title,
-    body
-  }
+    body,
+  };
 
   http.post("http://localhost:3000/posts", data)
-      .then(data =>  {
-        ui.showAlert('Post added', 'alert alert-success');
-        ui.clearFields();
-        getPosts()
-      })
-      .catch(err => console.log(err))
+    .then((data) => {
+      ui.showAlert("Post added", "alert alert-success");
+      ui.clearFields();
+      getPosts();
+    })
+    .catch((err) => console.log(err));
+}
+
+function deletePost(e) {
+  if (e.target.parentElement.classList.contains("delete")) {
+    const id = e.target.parentElement.dataset.id;
+    if (confirm("Are you sure?")) {
+      http.delete(`http://localhost:3000/posts/${id}`)
+        .then((data) => {
+          ui.showAlert("Post removed", "alert alert-success");
+          getPosts();
+        })
+        .catch((err) => console.log(err));
+    }
+  }
+
+  e.preventDefault();
 }
